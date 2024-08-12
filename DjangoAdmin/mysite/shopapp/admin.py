d337881176fa4536cf3a8fe50008fcaab4e8e1ae
bekkhan <israpal@bk.ru>
2024-08-13 00:11:56 +0300

    added homework_04

---
+++ DjangoAdmin/mysite/shopapp/admin.py
@@ -0,0 +1,88 @@
+from django.contrib import admin
+from django.http import HttpRequest
+from django.db.models import QuerySet
+
+from .models import Product, Order
+from .admin_mixins import ExportAsCSVMixin
+
+
+class OrderInline(admin.TabularInline):
+    model = Product.orders.through
+
+
+@admin.action(description="Archived products")
+def mark_archived(model_admin: admin.ModelAdmin, request: HttpRequest, queryset: QuerySet):
+    queryset.update(archived=True)
+
+
+@admin.action(description="Unarchived products")
+def mark_unarchived(model_admin: admin.ModelAdmin, request: HttpRequest, queryset: QuerySet):
+    queryset.update(archived=False)
+
+
+@admin.register(Product, ExportAsCSVMixin)
+class ProductAdmin(admin.ModelAdmin):
+    actions = [
+        mark_archived,
+        mark_unarchived,
+        "export_csv",
+    ]
+    inlines = [
+        OrderInline,
+    ]
+    list_display = "pk", "name", "short_description", "price", "discount", "created_at", "archived"
+    list_display_links = "pk", "name"
+    search_fields = "price", "name"
+    ordering = "pk", "name"
+    fieldsets = [
+        (None, {
+            "fields": ("name", "description"),
+        }),
+        ("Price options", {
+            "fields": ("price", "discount"),
+            "description": "Цена продукта с возможной скидкой.",
+        }),
+        ("Extra options", {
+            "fields": ("archived", ),
+            "classes": ("collapse", ),
+            "description": "Field 'archived' is for soft delete."
+        })
+    ]
+
+    @staticmethod
+    def short_description(obj: Product) -> str:
+        if len(obj.description) < 48:
+            return obj.description
+        return obj.description[:48] + "..."
+
+
+# class ProductInline(admin.StackedInline):
+class ProductInline(admin.TabularInline):
+    model = Order.products.through
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductInline,
+    ]
+    list_display = "pk", "delivery_address", "promo_code", "created_at", "user_verbose"
+    list_display_links = "pk", "user_verbose"
+    ordering = "pk", "created_at"
+    search_fields = "pk", "delivery_address"
+
+    def get_queryset(self, request):
+        return Order.objects.select_related("user").prefetch_related("products")
+
+    @staticmethod
+    def user_verbose(obj: Order) -> str:
+        return obj.user.first_name or obj.user.username
+
+
+# admin.site.register(Product, ProductAdmin)
+# admin.site.register(Order, OrderAdmin)
+
+
+@admin.action(description="Archived products")
+def mark_archived(model_admin: admin.ModelAdmin, request: HttpRequest, queryset: QuerySet):
+    queryset.update(archived=True)
---
+++ DjangoAdmin/mysite/shopapp/admin_mixins.py
@@ -0,0 +1,27 @@
+import csv
+
+from django.http import HttpRequest, HttpResponse
+from django.db.models import QuerySet
+from django.db.models.options import Options
+
+
+class ExportAsCSVMixin:
+    def export_csv(self, request: HttpRequest, queryset: QuerySet):
+        print("=" * 200)
+        meta: Options = self.model._meta
+        field_names = [f"{field.name}\t" for field in meta.fields]
+
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = f"attachment; filename={meta}-export.csv"
+
+        csv_writer = csv.writer(response)
+        csv_writer.writerow(field_names)
+
+        for obj in queryset:
+            csv_writer.writerow([f"{getattr(obj, field)}\t" for field in field_names])
+
+        return response
+
+    export_csv.short_description = "Export as CSV"
+
+
